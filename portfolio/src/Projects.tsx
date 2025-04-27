6940bd3f93ff72cae43ddeb8f7b66ae1186fc8c8
Garrett Thomas <garrett@Garretts-MacBook-Pro-7.local>
2025-04-26 19:26:37 -0600

    smart drone wasm now renders

--- portfolio/src/Projects.tsx
+++ portfolio/src/Projects.tsx
@@ -1,15 +1,41 @@
 
-
+import { useEffect } from "react";
 
 
 function Projects() {
 
+    useEffect(() => {
 
+        const setup = document.createElement('script');
+        const droneJS = document.createElement('script');
+
+        setup.src = "setup.js";
+        droneJS.src = "drone.js";
+
+        droneJS.defer = true;
+        droneJS.async = true;
+        document.body.appendChild(setup);
+        document.body.appendChild(droneJS);
+
+    }, []);
+
+    function handleContextMenu(e: { preventDefault: () => void; }) {
+
+        e.preventDefault();
+
+    }
     return (<>
 
-        <p>
-            My amazing projects:
-        </p>
+        <figure id="spinner">
+            <div className="spinner"></div>
+        </figure>
+        <div className="emscripten" id="status">Downloading...</div>
+        <div className="emscripten">
+            <progress value="0" max="100" id="progress" hidden={true}></progress>
+        </div >
+
+        <canvas className="emscripten" id="canvas" onContextMenu={handleContextMenu} tabIndex={-1}></canvas>
+        <script>alert('hello');</script>
     </>);
 
 
